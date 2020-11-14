d22d0d49f19715b62e57f43d798f6aecb57cbd24
Thiago Damaceno <tavaresdamaceno@gmail.com>
2020-11-14 10:16:58 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('#btn-login').onclick = () => {
+    document.querySelector('#container-login').classList.add('d-none')
+    document.querySelector('#container-principal').classList.remove('d-none')
+}
